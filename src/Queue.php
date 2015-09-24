8fd8d5c35a5bb1f4bbf4020685942994824aa67a
Riri <namespace@ph1.enthropia.com>
2015-09-24 23:16:31 +0800

    added some other options

--- src/Queue.php
+++ src/Queue.php
@@ -7,23 +7,34 @@
  * distributed with this package.
  */
 
-namespace Eve;
+namespace Eve\Framework;
 
 use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 /**
  * Queue Controller
  *
  * @package Eve
  */
-class Queue extends Base
+class Queue extends \Eve\Framework\Index
 {	
-	protected $connection;
+	protected $task;
+	protected $user;
+	protected $type;
+	protected $delay;
+	protected $appId;
+	protected $corrId;
 	protected $channel;
 	protected $message;
-	protected $priority;
-	protected $task;
-	protected $delay;
+	protected $replyTo;
+	protected $timestamp;
+	protected $connection;
+	protected $expiration;
+	protected $contentType;
+	protected $application;
+	protected $contentEncoding;
+	protected $retry = null;
 	protected $persistent = 2;
 	protected $priority = 'low';
 
@@ -32,9 +43,24 @@ class Queue extends Base
 		$this->connection = new AMQPConnection($host, $port, $username, $password);
 		$this->channel = $this->connection->channel();
 
-		if($task && $data) {
+		if ($task && $data) {
 			$this->setTask($task)->setData($data);
 		}
+	}
+
+	/**
+     * set Application
+     *
+     * @param *string
+     * @return this
+     */
+	public function setApplication($app)
+	{	
+		Argument::i()
+            ->test(1, 'string');
+
+		$this->application = $app;
+		return $this->setData($this->message);
 	}
 	
     /**
@@ -61,11 +87,29 @@ class Queue extends Base
 	public function setData($data)
 	{	
 		Argument::i()
-            ->test(1, 'array');
+            ->test(1, 'array');        
 
-		$data['task'] = $this->task;
-		$this->message = json_encode($data);
+		$data['TASK'] = $this->task;
+		$data['RETRY'] = $this->retry;
+		$data['APPLICATION'] = $this->application;
+
+		$this->message = $data;
 		return $this;
+	}
+
+	/**
+     * send data
+     *
+     * @param *int
+     * @return this
+     */
+	public function setRetry($retry)
+	{	
+		Argument::i()
+            ->test(1, 'int');
+
+		$this->retry = $retry;
+		return $this->setData($this->message);
 	}
 
     /**
@@ -96,11 +140,11 @@ class Queue extends Base
 
         switch ($priority) {
         	case 'high':
-        		$this->priority = 10;
+        		$this->priority = 9;
         		break;
 
         	case 'medium':
-        		$this->priority = 5;
+        		$this->priority = 4;
         		break;
 
         	case 'low':
@@ -128,6 +172,142 @@ class Queue extends Base
         $this->delay = $delay;
         return $this;
 	}
+
+	/**
+     * sets expiration in seconds
+     *
+     * @param *int
+     * @return this
+     */
+	public function setExpiration($sec)
+	{
+		Argument::i()
+            ->test(1, 'int');
+
+        $this->expiration = $sec;
+        return $this;
+	}
+
+	/**
+     * sets message content type
+     *
+     * @param *string
+     * @return this
+     */
+	public function setContentType($type)
+	{
+		Argument::i()
+            ->test(1, 'string');
+
+        $this->contentType = $type;
+        return $this;
+	}
+
+	/**
+     * sets message content encoding
+     *
+     * @param *string
+     * @return this
+     */
+	public function setContentEncoding($encoding)
+	{
+		Argument::i()
+            ->test(1, 'string');
+
+        $this->contentEncoding = $encoding;
+        return $this;
+	}
+
+	/**
+     * sets application id
+     *
+     * @param *string
+     * @return this
+     */
+	public function setAppId($appId)
+	{
+		Argument::i()
+            ->test(1, 'string');
+
+        $this->appId = $appId;
+        return $this;
+	}
+
+	/**
+     * sets correlation id
+     *
+     * @param *string
+     * @return this
+     */
+	public function setCorrelationId($corrId)
+	{
+		Argument::i()
+            ->test(1, 'string');
+
+        $this->corrId = $corrId;
+        return $this;
+	}
+
+	/**
+     * sets timestamp in secs
+     *
+     * @param *string
+     * @return this
+     */
+	public function setTimestamp($sec)
+	{
+		Argument::i()
+            ->test(1, 'string');
+
+        $this->timestamp = $timestamp;
+        return $this;
+	}
+
+	/**
+     * used to name a reply queue
+     *
+     * @param *string
+     * @return this
+     */
+	public function setReply($queue)
+	{
+		Argument::i()
+            ->test(1, 'string');
+
+        $this->replyTo = $replyTo;
+        return $this;
+	}
+
+	/**
+     * sets task/message type
+     *
+     * @param *string
+     * @return this
+     */
+	public function setType($type)
+	{
+		Argument::i()
+            ->test(1, 'string');
+
+        $this->type = $type;
+        return $this;
+	}
+
+	/**
+     * sets task/message sender
+     * the user used to create the channel
+     *
+     * @param *string
+     * @return this
+     */
+	public function setUserId($user)
+	{
+		Argument::i()
+            ->test(1, 'string');
+
+        $this->user = $user;
+        return $this;
+	}
 	
     /**
      * process the add queue request
@@ -142,12 +322,10 @@ class Queue extends Base
 		$this->channel->queue_bind('queue', 'queue-xchnge');
 
 		// set message
-		$msg = new AMQPMessage($this->message, array(
-			'delivery_mode' => $this->persistent, 
-			'priority' => $this->priority));
+		$msg = new AMQPMessage(json_encode($this->message), $this->setOptions());
 
 		// if no delay queue it now
-		if($this->delay) {	
+		if (!$this->delay) {	
 			// queue it up main queue container
 			$this->channel->basic_publish($msg, 'queue-xchnge');
 
@@ -182,5 +360,66 @@ class Queue extends Base
 		$this->channel->basic_publish($msg, 'xchnge-delay');
 
 		return $this;
+	}
+
+	/**
+     * set options
+     *
+     * @return array options
+     */
+	public function setOptions() {
+		$options = array('delivery_mode' => $this->persistent);
+
+		// if priority is set
+		if ($this->priority) {
+			$options['priority'] = $this->priority;
+		}
+
+		// if expiration is set
+		if ($this->expiration) {
+			$options['expiration'] = $this->expiration;
+		}
+
+		// if content type is set
+		if ($this->contentType) {
+			$options['content_type'] = $this->contentType;
+		}
+
+		// if content encoding is set
+		if ($this->contentEncoding) {
+			$options['content_encoding'] = $this->contentEncoding;
+		}
+
+		// if application id is set
+		if ($this->appId) {
+			$options['app_id'] = $this->appId;
+		}
+
+		// if user id is set
+		if ($this->user) {
+			$options['user_id'] = $this->user;
+		}
+
+		// if timestamp is set
+		if ($this->timestamp) {
+			$options['timestamp'] = $this->timestamp;
+		}
+
+		// if type is set
+		if ($this->type) {
+			$options['type'] = $this->type;
+		}
+
+		// if correlation id is set
+		if ($this->corrId) {
+			$options['correlation_id'] = $this->corrId;
+		}
+
+		// if correlation id is set
+		if ($this->replyTo) {
+			$options['reply_to'] = $this->replyTo;
+		}
+
+		return $options;
 	}
 }
