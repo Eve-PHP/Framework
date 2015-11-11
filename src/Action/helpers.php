7facd279fb2e6d58681e216abf3706cf479a2dd8
Christian Blanquera <cblanquera@openovate.com>
2015-11-12 00:30:43 +0800

    Critical update

--- src/Action/helpers.php
+++ src/Action/helpers.php
@@ -270,7 +270,7 @@ return array(
     'querystring' => function($key = null, $value = '') {
         $query = $_GET;
 		
-		if(!is_null($key)) {
+		if(is_scalar($key) && !is_null($key) && isset($query[$key])) {
 			$query[$key] = $value;
 			$query = http_build_query($query);
 			parse_str(urldecode($query), $query);
